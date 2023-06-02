51b334a14b2f8e7cbf5d4e2626637d913c13c86b
Alex Robin <alex.robin@sensiasoft.com>
2023-06-02 04:39:14 +0200

    deploy latest admin-console

---
+++ admin-console/public/ext/AddOn.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-card flat outlined class="pa-4">
+    <h2>More options for {{ config.name }}</h2>
+    <v-btn small>Extension Button</v-btn>
+  </v-card>
+</template>
+
+<script>
+
+module.exports = {
+  name: 'AddOn',
+  components: {    
+  },
+  props: ['config'],
+  data () {
+    return {       
+    }
+  },
+  computed: {    
+  },
+  methods: {
+  },
+  mounted() {    
+  },
+  destroyed() {    
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
