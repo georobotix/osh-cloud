7be711753878205d985055f7d66c2074f2afb291
mathieu dhainaut <mathieu.dhainaut@gmail.com>
2023-06-04 17:27:09 +0200

    update

---
+++ morpheusx/js/about-legacy.9158d75b.js
@@ -0,0 +1,43 @@
+"use strict";
+(self["webpackChunkvue3_webpack5"] = self["webpackChunkvue3_webpack5"] || []).push([[443],{
+
+/***/ 38423:
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "default": function() { return /* binding */ About; }
+});
+
+// EXTERNAL MODULE: ./node_modules/@vue/runtime-core/dist/runtime-core.esm-bundler.js
+var runtime_core_esm_bundler = __webpack_require__(66252);
+;// CONCATENATED MODULE: ./node_modules/webpack-plugin-vuetify/dist/scriptLoader.js!./node_modules/thread-loader/dist/cjs.js!./node_modules/babel-loader/lib/index.js??clonedRuleSet-40.use[1]!./node_modules/vue-loader/dist/templateLoader.js??ruleSet[1].rules[4]!./node_modules/vue-loader/dist/index.js??ruleSet[0].use[0]!./src/views/About.vue?vue&type=template&id=69c017c7
+
+var _hoisted_1 = {
+  class: "about"
+};
+var _hoisted_2 = /*#__PURE__*/(0,runtime_core_esm_bundler/* createElementVNode */._)("h1", null, "This is an about page", -1);
+var _hoisted_3 = [_hoisted_2];
+function render(_ctx, _cache) {
+  return (0,runtime_core_esm_bundler/* openBlock */.wg)(), (0,runtime_core_esm_bundler/* createElementBlock */.iD)("div", _hoisted_1, _hoisted_3);
+}
+;// CONCATENATED MODULE: ./src/views/About.vue?vue&type=template&id=69c017c7
+
+// EXTERNAL MODULE: ./node_modules/vue-loader/dist/exportHelper.js
+var exportHelper = __webpack_require__(83744);
+;// CONCATENATED MODULE: ./src/views/About.vue
+
+const script = {}
+
+;
+const __exports__ = /*#__PURE__*/(0,exportHelper/* default */.Z)(script, [['render',render]])
+
+/* harmony default export */ var About = (__exports__);
+
+/***/ })
+
+}]);
+//# sourceMappingURL=about-legacy.9158d75b.js.map
