bf1978ef9b5590f8af3edef40f4a34d6544a3f4a
mathieu dhainaut <mathieu.dhainaut@gmail.com>
2023-06-27 18:16:01 +0200

    Add version of 5 workers

--- morpheusx-dev/Workers/createBoxGeometry.js
+++ morpheusx-dev/Workers/createBoxGeometry.js
@@ -1,13 +1,1 @@
-/* This file is automatically rebuilt by the Cesium build process. */
-define(['./BoxGeometry-cbf2d438', './when-8166c7dd', './GeometryOffsetAttribute-e8e698d7', './RuntimeError-4fdc4459', './Transforms-62a339c3', './Matrix2-92b7fb9d', './ComponentDatatype-9ed50558', './WebGLConstants-0664004c', './combine-a5c4cc47', './GeometryAttribute-6f4c3b93', './GeometryAttributes-50becc99', './VertexFormat-c0801687'], function (BoxGeometry, when, GeometryOffsetAttribute, RuntimeError, Transforms, Matrix2, ComponentDatatype, WebGLConstants, combine, GeometryAttribute, GeometryAttributes, VertexFormat) { 'use strict';
-
-  function createBoxGeometry(boxGeometry, offset) {
-    if (when.defined(offset)) {
-      boxGeometry = BoxGeometry.BoxGeometry.unpack(boxGeometry, offset);
-    }
-    return BoxGeometry.BoxGeometry.createGeometry(boxGeometry);
-  }
-
-  return createBoxGeometry;
-
-});
+define(["./BoxGeometry-cbf2d438","./when-8166c7dd","./GeometryOffsetAttribute-e8e698d7","./RuntimeError-4fdc4459","./Transforms-62a339c3","./Matrix2-92b7fb9d","./ComponentDatatype-9ed50558","./WebGLConstants-0664004c","./combine-a5c4cc47","./GeometryAttribute-6f4c3b93","./GeometryAttributes-50becc99","./VertexFormat-c0801687"],(function(e,t,r,o,c,n,m,f,i,a,b,d){"use strict";function u(r,o){return t.defined(o)&&(r=e.BoxGeometry.unpack(r,o)),e.BoxGeometry.createGeometry(r)}return u}));
